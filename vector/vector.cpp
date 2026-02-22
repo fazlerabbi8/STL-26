8caf8fb1a42d4a54139a8bcde9ef3e64f161fbb8
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-22 18:48:57 +0600

    find font and back element of vector.

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,12 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     vector<int> arr;
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         arr.push_back(i);
     }
 
-    
+    for (auto it = arr.begin(); it != arr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << "\n\n";
+    cout << "------------------";
+    cout << "\n\n";
+    cout << arr.front() << "\n";
+    cout << arr.back() << "\n";
+
     return 0;
 }
