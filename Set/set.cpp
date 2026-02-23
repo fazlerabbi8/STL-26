a714a1611139267077e1f9533fe5d52f3701965a
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-23 17:21:10 +0600

    check 7 is exist or not in the set.

--- Set/set.cpp
+++ Set/set.cpp
@@ -10,6 +10,19 @@ int main(){
     for(auto it = s.begin(); it != s.end(); it++){
         cout << *it << " ";
     }
+
+    cout << "\n";
+
+    int n = 7;
+    if(s.find(7) != s.end()){
+        cout << n << " " << "is present in set." << "\n";
+    }
+
     
+
+    for(auto it = s.begin(); it != s.end(); it++){
+        cout << *it << " ";
+    }
+
     return 0;
 }
