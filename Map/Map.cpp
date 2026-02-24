1727f0bf4746fc67032b583b3be9b77460640d6d
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-24 12:06:14 +0600

    check 4 is exist in map or not.

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -1,15 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     map<int, int> mp;
-    for (int i = 1; i <= 5; i++){
-        mp.insert({i , i * 10});
+    for (int i = 1; i <= 5; i++)
+    {
+        mp.insert({i, i * 10});
     }
 
-    for (auto it = mp.begin(); it != mp.end(); it++){
-        cout << it -> first << "\t" << it -> second << "\n";
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << "\t" << it->second << "\n";
     }
 
+    int n = 4;
+    if (mp.find(4) != mp.end())
+    {
+        cout << n << " is present in map" << "\n";
+    }
+
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << "\t" << it->second << "\n";
+    }
     return 0;
 }
