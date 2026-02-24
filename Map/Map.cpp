8d7b076f7a827d0bf522af113a3c2b18ed125f9b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-24 12:11:49 +0600

    erase element from map.

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -14,13 +14,22 @@ int main()
         cout << it->first << "\t" << it->second << "\n";
     }
 
-    int n = 4;
-    if (mp.find(4) != mp.end())
+    int n = 6;
+    if (mp.find(n) != mp.end())
     {
         cout << n << " is present in map" << "\n";
+    }else{
+        cout << n << " is not present in map" << "\n";
     }
 
-    for (auto it = mp.begin(); it != mp.end(); it++)
+    // for (auto it = mp.begin(); it != mp.end(); it++)
+    // {
+    //     cout << it->first << "\t" << it->second << "\n";
+    // }
+
+    mp.erase(next(mp.begin(), 2));
+
+     for (auto it = mp.begin(); it != mp.end(); it++)
     {
         cout << it->first << "\t" << it->second << "\n";
     }
