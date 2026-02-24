185bfef29b62809e0f0c48b3fe2b27d3622a601b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-24 15:45:37 +0600

    Push the elements in the queue and print all elements.

---
+++ Queue/queue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(queue<int> q1){
+    queue<int> q2 = q1;
+    while(!q2.empty()){
+        cout << q2.front() << "\n";
+        q2.pop();
+    }
+}
+
+int main(){
+    queue<int> q;
+    for(int i = 1; i <= 5; i++){
+        q.push(i);
+    }
+
+    print(q);
+    
+    return 0;
+}
