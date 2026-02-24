ae2235c657219c944c38970c8d20232c637890a6
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-24 15:51:27 +0600

    find first and last element of the queue.

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -1,22 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print(queue<int> q1){
+void print(queue<int> q1)
+{
     queue<int> q2 = q1;
-    while(!q2.empty()){
+    while (!q2.empty())
+    {
         cout << q2.front() << "\n";
         q2.pop();
     }
 }
 
-int main(){
+int main()
+{
     queue<int> q;
-    for(int i = 1; i <= 5; i++){
+    for (int i = 1; i <= 5; i++)
+    {
         q.push(i);
     }
 
     print(q);
 
     cout << "size of the queue: " << q.size() << "\n";
+    cout << "The front element of the queue: " << q.front() << "\n";
+    cout << "The last element of the queue: " << q.back() << "\n";
+
+    
     return 0;
 }
