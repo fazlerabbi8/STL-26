830313ee92588cd41c2f6316c0c6dbc732b6d516
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-03 20:18:52 +0600

    unpaking the pair.split pair into two separate variables.

--- Pair/Pair.cpp
+++ Pair/Pair.cpp
@@ -6,5 +6,9 @@ int main(){
     cin >> p.first >> p.second;
     cout << p.first << " " << p.second << "\n";
     
+
+    auto [number, text] = p;
+    cout << "Number: " << number << "\n";
+    cout << "Number: " << text << "\n";
     return 0;
 }
