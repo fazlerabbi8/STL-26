1fe0f80209e83a77b475e2a1730976c5d6c6c737
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-03 20:01:29 +0600

    take input of pair.first value is integer and second value is string.

---
+++ Pair/Pair.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int, string> p;
+    cin >> p.first >> p.second;
+    cout << p.first << " " << p.second << "\n";
+    
+    return 0;
+}
