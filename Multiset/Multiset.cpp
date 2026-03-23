d312dc0448c544de6c6a4819856382893a9aee86
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-23 18:56:33 +0600

    count how many times 7 in multiset.

--- Multiset/Multiset.cpp
+++ Multiset/Multiset.cpp
@@ -1,15 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
+int main()
+{
+    // int n;
+    // cin >> n;
 
     multiset<int> ms;
-    for(int i = 0; i < n; i++){
-        int x;
-        cin >> x;
-        ms.insert(x);
-    }
+    // for(int i = 0; i < n; i++){
+    //     int x;
+    //     cin >> x;
+    //     ms.insert(x);
+    // }
+    ms.insert(5);
+    ms.insert(3);
+    ms.insert(3);
+    ms.insert(7);
+    ms.insert(3);
 
+    // auto it = ms.find(7);
+
+    // if (it != ms.end())
+    // {
+    //     cout << *it;
+    // }
+    // cout << "\n";
+
+    cout << ms.count(7) << "\n";
+
+    for (auto x : ms)
+    {
+        cout << x << " ";
+    }
 }
