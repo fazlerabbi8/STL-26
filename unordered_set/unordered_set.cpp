421b9b30c36db325afbbeed5eea036b3df1337c2
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-22 20:00:01 +0600

    declare unordered set.

---
+++ unordered_set/unordered_set.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> u_set;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        u_set.insert(x);
+    }
+
+    for(auto x : u_set)
+    {
+        cout << x << "\n";
+    }
+    return 0;
+}
