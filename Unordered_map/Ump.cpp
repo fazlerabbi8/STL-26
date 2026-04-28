856dd1816ab278bec8fccafc2678596694b07072
fazlerabbi13 <mohammad0132794@gmail.com>
2026-04-28 21:02:39 +0600

    declare unordered map and storing data.

--- Unordered_map/Ump.cpp
+++ Unordered_map/Ump.cpp
@@ -5,24 +5,28 @@ int main()
 {
     vector<int> a = {1, 2, 2, 3, 3, 3};
 
-    unordered_map<int, int> freq;
+    // unordered_map<int, int> freq;
 
-    for (int x : a)
-    {
-        freq[x]++;
-    }
+    // for (int x : a)
+    // {
+    //     freq[x]++;
+    // }
 
-    for (auto it : freq)
-    {
-        cout << it.first << " " << it.second << endl;
-    }
+    // for (auto it : freq)
+    // {
+    //     cout << it.first << " " << it.second << endl;
+    // }
 
-    if (freq.find(3) == freq.end())
-    {
-        cout << "Key is not present." << "\n";
-    }
-    else
-    {
-        cout << "Key is present." << "\n";
-    }
+    // if (freq.find(3) == freq.end())
+    // {
+    //     cout << "Key is not present." << "\n";
+    // }
+    // else
+    // {
+    //     cout << "Key is present." << "\n";
+    // }
+
+    unordered_map<string, int>
+    umap, myBooks ={{"Java", 2}, {"C++", 2}};
+    
 }
