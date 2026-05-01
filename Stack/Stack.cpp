eded283c9afd974d33808bd7bf6baf04d3253b42
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-01 20:37:59 +0600

    take stack elements from user.

---
+++ Stack/Stack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> myStack;
+    int n;
+
+    cout << "Enter Number of Elements: ";
+    cin>> n;
+
+    cout << "Enter Elements: ";
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        myStack.push(x);
+    }
+
+    
+}
