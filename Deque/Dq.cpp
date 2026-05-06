97211c3311c5b86856ec0e0c645fcfec444c1a15
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-06 19:29:05 +0600

    add element in deque.

--- Deque/Dq.cpp
+++ Deque/Dq.cpp
@@ -5,11 +5,13 @@ int main()
 {
     deque<int> nums {2, 3};
 
+    nums.push_back(4);
+    nums.push_back(7);
+    nums.push_back(8);
+
     // print deque
     for(auto x: nums)
     {
-        cout << num << " ";
+        cout << x << " ";
     }
-
-    nums.push_back(4);
 }
